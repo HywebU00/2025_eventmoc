cb8d0c6c637b8517690660de63a1c057394fece8
Lize Wu <retniw72@gmail.com>
2025-08-25 16:40:10 +0800

    table scroll debug

--- js/customize.js
+++ js/customize.js
@@ -584,3 +584,82 @@ $(function () {
       });
   }
 });
+
+
+// hyui js
+/*-----------------------------------*/
+///////////////// 變數 ////////////////
+/*-----------------------------------*/
+var _window = $(window),
+    ww = _window.outerWidth(),
+    wh = _window.height(),
+    _body = $('body'),
+    wwNormal = 1400,
+    wwMedium = 992,
+    wwSmall = 768,
+    wwxs = 576;
+/*-------------------------------------*/
+/////////table 加上響應式table wrapper/////
+///*------------------------------------*/
+$('table').each(function(index, el) {
+    //判斷沒有table_list
+    if ($(this).parents('.table_list').length == 0 && $(this).parents('.fix_th_table').length == 0 && $(this).parent('form').length == 0) {
+        $(this).scroltable();
+    }
+});
+// tablearrow arrow，為了設定箭頭
+$('.scroltable-nav-left').append('<div class="tablearrow_left" style="display:block;"></div>');
+$('.scroltable-nav-right').append('<div class="tablearrow_right"  style="display:block;"></div>');
+
+// 固定版頭
+function table_Arrow() {
+    if ($('table').parents('.table_list').length == 0 && $(this).parent('form').length == 0) {
+        if ($('.scroltable-wrapper').length > 0) {
+            var stickyArrowTop = Math.floor($('.scroltable-wrapper').offset().top),
+                thisScroll = Math.floor($(this).scrollTop());
+            if (thisScroll > stickyArrowTop - 230) {
+                $('.scroltable-wrapper .tablearrow_left').css('display', 'block');
+                $('.scroltable-wrapper .tablearrow_left').css({ "top": thisScroll - stickyArrowTop + 220 }, 100, 'easeOutQuint');
+                $('.scroltable-wrapper .tablearrow_right').css('display', 'block');
+                $('.scroltable-wrapper .tablearrow_right').css({ "top": thisScroll - stickyArrowTop + 220 }, 100, 'easeOutQuint');
+                // $('.scroltable-wrapper .tablearrow_right').css({ "top": thisScroll - stickyArrowTop + 220 }, 100, 'easeOutQuint');
+            } else {
+                $('.scroltable-wrapper .tablearrow_left').css({
+                    top: '10px',
+                    display: 'none'
+                });
+                $('.scroltable-wrapper .tablearrow_right').css({
+                    top: '10px',
+                    display: 'none'
+                });
+            }
+        }
+    }
+}
+$(window).scroll(function(event) {
+    table_Arrow();
+});
+var scrollTimer;
+_window.scroll(function() {
+    clearTimeout(scrollTimer);
+    scrollTimer = setTimeout(function() {
+        table_Arrow();
+    }, 50);
+});
+///*------------------------------------*/
+////////////table 加上 data-title//////////
+///*------------------------------------*/
+function rwdTable() {
+    $('.table_list').find('table').each(function() {
+        var $row = $(this).find('tr');
+        rowCount = $row.length;
+        for (var n = 1; n <= rowCount; n++) {
+            $(this).find('th').each(function(index) {
+                var thText = $(this).text();
+                $row.eq(n).find('td').eq(index).attr('data-title', thText);
+            });
+        }
+    });
+}
+rwdTable();
+
